bb86ffc7949ebccc6c58be42f0be5a9843de926a
ouissal rms <khramswissal@gmail.com>
2025-12-16 21:25:33 +0100

    fin de projet

--- src/App.js
+++ src/App.js
@@ -1,86 +1,110 @@
-
 import './App.css';
+import axios from 'axios';
 import Typography from '@mui/material/Typography';
-//matriel UI component
 import Container from '@mui/material/Container';
-//Icon
 import CloudIcon from '@mui/icons-material/Cloud';
-//translition
 import Button from '@mui/material/Button';
+import moment from 'moment/moment';
+import "moment/min/locales";
+
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
+moment.locale("ar");
+let Cancelaxios = null;
+
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const [date, setDate] = useState("");
+  const [temp, setTemp] = useState({
+    Number: null,
+    description: "",
+    min: null,
+    max: null,
+    icon: null,
+  });
+
+  const [local, setLocal] = useState("ar");
+
+  const change = () => {
+    const newLang = local === "en" ? "ar" : "en";
+    setLocal(newLang);
+    i18n.changeLanguage(newLang);
+    moment.locale(newLang);
+    setDate(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(local);
+    setDate(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    axios.get(
+      'https://api.openweathermap.org/data/2.5/weather?lat=33.5731&lon=-7.5898&appid=710c525ed819a5558408251ad244e69a',
+      {
+        cancelToken: new axios.CancelToken(c => {
+          Cancelaxios = c;
+        })
+      }
+    )
+    .then(res => {
+      setTemp({
+        Number: Math.round(res.data.main.temp - 272.15),
+        min: Math.round(res.data.main.temp_min - 272.15),
+        max: Math.round(res.data.main.temp_max - 272.15),
+        description: res.data.weather[0].description,
+        icon: `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+      });
+    })
+    .catch(err => console.log(err));
+
+    return () => Cancelaxios && Cancelaxios();
+  }, []);
+
   return (
     <div className="App">
       <Container maxWidth="sm">
-        {/*  container content  */}
-        <div style={{height:"100vh",flexDirection:"column",color:"white" ,display:"flex" ,justifyContent:"center" ,alignItems:"center"}}>
-        {/*  CARD   */}
-             <div dir='rtl' style={{background:"#3c5b7188"
-             ,color:"white" ,padding:"10px" 
-              ,borderRadius:"15px",
-              width:"100%",
-              boxShadow:"0px 11px 1px rgba(0,0,0,0.05)"}}>
-               {/*  conetent   */}
-              <div>
-                 {/*  city And Time   */}
-                 <div style={{display:"flex",alignItems:"end" ,justifyContent:"start"
-                   ,direction:"rtl"}}>
-                     <Typography variant="h2" style={{margin:"20px"}}>
-                    الدار البيضاء
-                     </Typography>
-                     <Typography variant="h5" style={{margin:"20px"}}>
-                   يوم الأربعاء
-                     </Typography>
-                 </div>
-                    {/*  city And Time   */}
-                    <hr></hr>
-                    {/*  dgree And description  */}
-                      {/* container of  dgree And description  */}
-                    <div style={{display:"flex" ,justifyContent:"space-around"}}>
-                    <div>
-                       {/*  Temp  */}
-                       <div>
-                          <Typography variant="h1" style={{textAlign:"right"}}>
-                          38
-                            </Typography>
-                              {/* todo :image Temp  */}
-                               {/* todo :image Temp  */}
+        <div className="weather-wrapper">
 
-                       </div>
-                        {/*  Temp  */}
-                           <Typography variant="h6" style={{textAlign:"center"}}>
-                           browen clouds
-                            </Typography>
-                             {/*  min and max  */}
-                             <div style={{textAlign:"center" ,display:"flex" ,justifyContent:"space-between",alignItems:"center"}}>
-                              <h5> 38 : صغير</h5>
-                               <h5> |</h5>
-                               <h5> 38 :كبيرة</h5>
-                             </div>
-                        
-                      
-                    </div>
-                    {/*  dgree And description  */}
+          <div className="weather-card" dir={local === "ar" ? "rtl" : "ltr"}>
 
-                    <CloudIcon style={{fontSize:"200px" ,color:"white"}}></CloudIcon>
-                    
-                    </div>
-                     {/* container of  dgree And description  */}
-
-              </div>
-              {/*  conetent   */}
-             </div>
-         {/* FIN CARD   */}
-             {/* Translition container   */}
-             <div
-             dir='rtl'  style={{display:"flex" ,justifyContent:"end",width:"100%"}}>
-              <Button variant="text">English</Button>
-             </div>
-               {/* Translition container   */}
+            <div className="city-time">
+              <Typography variant="h3">{t("Casablanca")}</Typography>
+              <Typography variant="h5">{date}</Typography>
             </div>
-         {/*  container content  */}
-      
+
+            <hr />
+
+            <div className="weather-content">
+              <div>
+                <div className="temp-main">
+                  <Typography variant="h1">{temp.Number}</Typography>
+                  {temp.icon && <img src={temp.icon} alt="weather" />}
+                </div>
+
+                <Typography variant="h6" className="temp-description">
+                  {t(temp.description)}
+                </Typography>
+
+                <div className="temp-minmax">
+                  <span>{t("Min")} {temp.min}°</span>
+                  <span>|</span>
+                  <span>{t("Max")} {temp.max}°</span>
+                </div>
+              </div>
+
+              <CloudIcon className="cloud-icon" />
+            </div>
+          </div>
+
+          <div className="lang-switch">
+            <Button onClick={change}>
+              {local === "en" ? "English" : "Arabic"}
+            </Button>
+          </div>
+
+        </div>
       </Container>
-   
     </div>
   );
 }
